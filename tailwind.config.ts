2861483b2b05b9ab345a48eef4223f018780539f
Dylan Reed <dylan.reeder5@gmail.com>
2024-05-09 11:17:09 -0400

    marquee speed slower

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -84,7 +84,7 @@ export default withUt({
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
-        "marquee-infinite": "marquee 45s linear infinite",
+        "marquee-infinite": "marquee 400s linear infinite",
       },
     },
   },
