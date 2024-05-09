af28948b5d03e4a5ac7c4b1f8e2755cf66930c82
Dylan Reed <dylan.reeder5@gmail.com>
2024-05-09 08:14:11 -0400

    bit more copy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     <>
       <section className="mx-auto text-center">
         <h1 className="mx-auto max-w-lg text-4xl font-semibold">
-          Get your grades easier with science and AI.
+          Get the grades you need easier with science and AI.
         </h1>
         <p className="text-lg text-foreground/85">
           Upload your lecture slides and study AI-powered quiz and flashcards.
@@ -21,6 +21,22 @@ export default function Home() {
         </div>
       </section>
       <SuggestionsList />
+      <section className="space-y-2 text-center">
+        <h2 className="text-2xl font-semibold">
+          AI-powered assessments custom from your lectures
+        </h2>
+        <p className="text-foreground/85">
+          We use AI to generate quizzes and flashcards from your lecture slides.
+          We also generate keywords for your exam to help you study better.
+        </p>
+      </section>
+      <section className="space-y-2 text-center">
+        <h3 className="text-2xl font-semibold">How it works</h3>
+        <p className="text-foreground/85">
+          Upload your lecture slides, and we will generate quizzes and
+          flashcards for you to study.
+        </p>
+      </section>
     </>
   );
 }
