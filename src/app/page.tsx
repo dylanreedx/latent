54cd3976698f0022bc301547192556dfadfd2e43
Dylan Reed <dylan.reeder5@gmail.com>
2024-05-10 07:58:32 -0400

    better copy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -47,19 +47,18 @@ export default function Home() {
       </section>
       <SuggestionsList />
       <section className="space-y-2 text-center">
-        <h2 className="text-2xl font-semibold">
-          AI-powered assessments custom from your lectures
-        </h2>
+        <h2 className="text-2xl font-semibold">Save time for studying</h2>
         <p className="text-foreground/85">
-          We use AI to generate quizzes and flashcards from your lecture slides.
-          We also generate keywords for your exam to help you study better.
+          Focus on studying, not creating quizzes and flashcards. Let Latent do
+          the work for you.
         </p>
       </section>
       <section className="space-y-2 text-center">
-        <h3 className="text-2xl font-semibold">How it works</h3>
+        <h3 className="text-2xl font-semibold">You can be more productive</h3>
         <p className="text-foreground/85">
-          Upload your lecture slides, and we will generate quizzes and
-          flashcards for you to study.
+          While creating flashcards and quizzes feel like studying, it&apos;s
+          not. Be more productive by studying instead of creating study
+          materials.
         </p>
       </section>
       <section>
